11b642e201364f95f654f3b54a4b6f72cb69f4a2
t1nPlate <dimaveseliy12@gmail.com>
2022-07-20 23:33:13 +0300

    added example entry to file

--- src/com/calculator/Calculator.java
+++ src/com/calculator/Calculator.java
@@ -16,10 +16,12 @@ public class Calculator {
         FileWriter fw = new FileWriter(fileOutput, false);
         Scanner sc = new Scanner(fl);
 
-        result = calculate(sc.nextLine());
-        fw.write(result + "\n");
-        fw.flush();
-        System.out.println(result);
+        while(sc.hasNext()) {
+            result = calculate(sc.nextLine(), fw);
+            fw.write(result + "\n");
+            fw.flush();
+            System.out.println(result);
+        }
     }
 
     static double division(double a, double b) throws Exception {
@@ -27,33 +29,49 @@ public class Calculator {
         return a/b;
     }
 
-    static String calculate(String str) {
+    static String calculate(String str, FileWriter fw) throws IOException {
         String[] operation = str.split(" ");
-        String result = "";
+        String result;
         double a, b;
         try {
             a = Double.parseDouble(operation[0]);
             b = Double.parseDouble(operation[2]);
         } catch (IllegalArgumentException ex) {
             result = "Error! Not number";
+            fw.write(str + " = ");
             return result;
         } catch (NoSuchElementException ex) {
             result = "Empty line";
+            fw.write(str + " = ");
             return result;
         }
         switch (operation[1]) {
-            case "+" : result = String.valueOf(Math.round(Double.sum(a, b) * 100) / 100.0); break;
-            case "-" : result = String.valueOf(Math.round((a - b) * 100) / 100.0); break;
-            case "*" : result = String.valueOf(Math.round((a * b) * 100) / 100.0); break;
+            case "+" :
+                result = String.valueOf(Math.round(Double.sum(a, b) * 100) / 100.0);
+                fw.write(str + " = ");
+                break;
+            case "-" :
+                result = String.valueOf(Math.round((a - b) * 100) / 100.0);
+                fw.write(str + " = ");
+                break;
+            case "*" :
+                result = String.valueOf(Math.round((a * b) * 100) / 100.0);
+                fw.write(str + " = ");
+                break;
             case "/" :
                 try {
                     result = String.valueOf(division(a, b));
+                    fw.write(str + " = ");
                     break;
                 } catch (Exception ex) {
-                    System.out.println(ex.getMessage());
+                    result = ex.getMessage();
+                    fw.write(str + " = ");
                     break;
                 }
-            default : result = "Operation Error!"; break;
+            default :
+                result = "Operation Error!";
+                fw.write(str + " = ");
+                break;
         }
         return result;
     }
